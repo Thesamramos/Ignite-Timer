d4bfcde694f10388b50e00ca65385f3652bffd17
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2023-12-14 15:00:07 -0400

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import {ThemeProvider} from "styled-components"
 import { Button } from "./components/Button";
+
+
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
@@ -11,6 +14,8 @@ export function App() {
       <Button variant="danger" />
       <Button variant="success" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
